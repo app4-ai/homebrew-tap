ba501b02d9debb3652c314b582707161d0730166
Ersin Isimtekin <ersin.isimtekin@finbyte.com>
2026-09-05 11:25:20 +0200

    chore(app4-cli): bump to v1.28.4 (#12)
    Co-authored-by: app4-release-bot <release-bot@app4.dev>

--- Formula/app4-cli.rb
+++ Formula/app4-cli.rb
@@ -7,28 +7,28 @@
 class App4Cli < Formula
   desc "Command-line interface for the App4 platform"
   homepage "https://app4.dev"
-  version "1.28.3"
+  version "1.28.4"
   license "Apache-2.0"
 
   on_macos do
     on_arm do
-      url "https://s3.app4.studio/app4-studio/cli/app4-v1-28-3-darwin-arm64.gz"
-      sha256 "d162ba38c8df226bf2e64a99ea31b418c912cb4ed74051d538028f0dcd87e9c3"
+      url "https://s3.app4.studio/app4-studio/cli/app4-v1-28-4-darwin-arm64.gz"
+      sha256 "60c0f7f0e6bb14f588326fb90a8b6f3ff1256f690889dc8798046ee2161bfd51"
     end
     on_intel do
-      url "https://s3.app4.studio/app4-studio/cli/app4-v1-28-3-darwin-amd64.gz"
-      sha256 "a2622edbf25caccbdb1a1104fbbd258c5901619bdfc3c7040aa9a82d10660a58"
+      url "https://s3.app4.studio/app4-studio/cli/app4-v1-28-4-darwin-amd64.gz"
+      sha256 "822ffb7dfa30ce1135073d044ccaa9610ded86e3c19def14f38d43fb8239a384"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://s3.app4.studio/app4-studio/cli/app4-v1-28-3-linux-arm64.gz"
-      sha256 "8f8337c2d096efa21e8dd0b0f3402a4a148ab621038aa9012228227657984c8d"
+      url "https://s3.app4.studio/app4-studio/cli/app4-v1-28-4-linux-arm64.gz"
+      sha256 "02ac89d7eccc419c51de8360641f6bbb4204adafe30557de59dbe30c62ff14d0"
     end
     on_intel do
-      url "https://s3.app4.studio/app4-studio/cli/app4-v1-28-3-linux-amd64.gz"
-      sha256 "df29c11a741025dd2fd0d7e3d06d57fef44106273a932bec174f1fcf01721991"
+      url "https://s3.app4.studio/app4-studio/cli/app4-v1-28-4-linux-amd64.gz"
+      sha256 "745facc6a486a97b6160f44a4a94490003261a1b62f13aca88b540c52ddde097"
     end
   end
 
