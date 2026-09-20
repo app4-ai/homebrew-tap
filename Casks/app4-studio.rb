99d9cb7b5d88798d208dfaa591aabbfa276e803d
Ersin Isimtekin <ersin.isimtekin@finbyte.com>
2026-09-20 19:47:13 +0200

    chore(app4-studio): bump to 0.1.225 (#119)

--- Casks/app4-studio.rb
+++ Casks/app4-studio.rb
@@ -4,8 +4,8 @@
 # Rendered by `make release:desktop` in app4-smart-studio from
 # smart-studio/desktop/manifest.json. Do not edit by hand.
 cask "app4-studio" do
-  version "0.1.224"
-  sha256 "54a9631e8d9fb0b289624eadfb0a0983e1db4a4d5b9c1061f2ff65cc5a98f2d8"
+  version "0.1.225"
+  sha256 "8021bed9225c32c58a023762b050cd0c254fafc5afb9bb25bf04dacb07d1d827"
 
   url "https://s3.app4.studio/app4-studio/smart-studio/desktop/App4Studio-#{version}.dmg"
   name "App4 Studio"
